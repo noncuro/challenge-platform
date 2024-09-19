dcd1c2aa2d6c0f4da32409aa5a86f907951004e2
Daniel Cahn <daniel@slingshot.xyz>
2024-09-19 19:28:40 -0400

    Minor edits

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -12,7 +12,6 @@ export default async function AdminLayout({
 }) {
 
   const adminAuthKey = cookies().get('adminAuthKey')?.value;
-  console.log(adminAuthKey);
 
   if (!adminAuthKey) {
     return <Login />;
